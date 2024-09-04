41a383b35f7a602523e042c491773793019aafb0
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-09-04 17:59:56 +0530

    feat: Sidebar component Added

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,18 +1,22 @@
-"use client"
+"use client";
 
+import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
 
 interface WorkspaceIdLayoutProps {
-    children: React.ReactNode;
-};
+  children: React.ReactNode;
+}
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
-    return ( 
-        <div className="h-full">
-            <Toolbar />
-            {children}
-        </div>
-    );
-}
- 
+  return (
+    <div className="h-full">
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        {children}
+      </div>
+    </div>
+  );
+};
+
 export default WorkspaceIdLayout;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,9 @@
 "use client"
 
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-    const workspaceId = useWorkspaceId();
-    const { data } = useGetWorkspace({ id: workspaceId });
-
     return ( 
         <div>
-            data: {JSON.stringify(data)}
+            Workspace id page
         </div>
     );
 }
