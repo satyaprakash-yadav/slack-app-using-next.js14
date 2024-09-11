e96a31b445cc1bbba658001943e90df944781132
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-09-11 23:39:41 +0530

    feat: Channel header & functionality Added

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,10 +1,41 @@
 "use client";
 
+import { Loader, TriangleAlert } from "lucide-react";
+
+import { UseGetChannel } from "@/features/channels/api/use-get-channel";
+
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Header } from "./header";
 
 const ChannelIdPage = () => {
+  const channelId = useChannelId();
+
+  const { data: channel, isLoading: channelLoading } = UseGetChannel({
+    id: channelId,
+  });
+
+  if (channelLoading) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center">
+        <Loader className="size-5 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!channel) {
+    return (
+      <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+        <TriangleAlert className="size-6 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">
+          Channel not found.
+        </span>
+      </div>
+    );
+  }
+
   return (
-    <div>
-        Channel Id Page!
+    <div className="flex flex-col h-full">
+      <Header title={channel.name} />
     </div>
   );
 };
