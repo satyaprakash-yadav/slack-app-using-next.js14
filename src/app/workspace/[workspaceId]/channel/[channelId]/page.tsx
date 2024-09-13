acd462af8954da054f343048f8d6934185d36644
“satyaprakash-yadav” <satyay9798@gmail.com>
2024-09-13 21:28:19 +0530

    feat: Editor component Added

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -6,6 +6,7 @@ import { UseGetChannel } from "@/features/channels/api/use-get-channel";
 
 import { useChannelId } from "@/hooks/use-channel-id";
 import { Header } from "./header";
+import { ChatInput } from "./chat-input";
 
 const ChannelIdPage = () => {
   const channelId = useChannelId();
@@ -36,6 +37,8 @@ const ChannelIdPage = () => {
   return (
     <div className="flex flex-col h-full">
       <Header title={channel.name} />
+      <div className="flex-1" />
+      <ChatInput placeholder={`Message # ${channel.name}`} />
     </div>
   );
 };
